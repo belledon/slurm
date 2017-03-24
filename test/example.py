a2ae5d4a51fecd1d0da3cbbabdae6e785f6136dd
belledon <mbelledonne@gmail.com>
2017-03-24 12:46:59 -0400

    updated

--- test/example.py
+++ test/example.py
@@ -1,8 +1,35 @@
+# THIS IS NOT INTENDED TO RUN AS IS, THE IMPORTS WILL FAIL.
+# PLEASE READ THE COMMENTS
+
+
 import argparse
 import slurm as om
-# Note: It is recommended to add this a submodule within that path
+# Note: The slurm repo has to be added as a submodule within that path
 # that you will be working in. Thus you'll use something like:
-#  ` from slurm import slurm`
+#  ` import slurm as om` were slurm is this package's name.
+
+# Below is an example package layout:
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
+# MyPackage:
+#   __init__.py
+#   \foo:
+#       __init__.py
+#       example.py (this file)
+#
+#   \slurm (This module as a submodule)
+#
+#   \bar:
+#        __init__.py
+#       bar.py
+#
+#   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #   #
+#
+# Ensuring that the path is known:
+#   export PYTHONPATH=<Path to MyPackage>
+#
+# You can now run the example as:
+#   python ../foo/example.py ....
+#
 # Also don't forget your __init__.py's!
 
 def main():
@@ -18,7 +45,7 @@ def main():
     # Pass the parser to slurm so it can add its own optional parameters
     # such as cpu, memory...
     # Call `python example.py -h` to see these arguments
-    args = slurm.args(parser).parse_args()
+    args = om.slurm.args(parser).parse_args()
 
     # Now lets define any job-inpendent arguments (to be applied to all jobs)
 
@@ -42,7 +69,8 @@ def main():
     # Finally the actual execution command.
     # Note this could be a really complicated function call, say to
     # a singularity container, matlab, python, or even better, a python
-    # environment inside a singularity container.
+    # environment inside a singularity container (hypothetically defined in
+    # bar.py)
 
     func = "echo $NAME really loves $FOOD"
 
@@ -52,7 +80,7 @@ def main():
     batches = [["For the {0:d}th time...".format(i)] for i in range(10)]
 
     # Constructs the batch object
-    batch = slurm.Batch(interpreter, modules, exports, func, batches, flags,
+    batch = om.slurm.Batch(interpreter, modules, exports, func, batches, flags,
         args)
     # Submit the jobarray
     batch.run()
