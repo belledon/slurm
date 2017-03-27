b1ac324f7eb1583abfcf0c4d20e5e271afff3a53
belledon <mbelledonne@gmail.com>
2017-03-27 17:39:58 -0400

    restructure imports

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 
-from src import parallelGlobals
-from src import job
-from src import slurm
-# from slurm.src import job
-# from slurm.src import parallelGlobals
-# from slurm.src import slurm
+# from src import parallelGlobals
+# from src import job
+# from src import slurm
+from .src import job
+from .src import parallelGlobals
+from .src import slurm
+
+__all__ = ["run"]
